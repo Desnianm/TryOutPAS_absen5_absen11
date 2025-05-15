eb8ab6542eaa704a00f3db6d738af4ce37d6849c
Danis <05548@smkrus.sch.id>
2025-05-15 20:45:35 +0700

    feat [MI] bottom-nav

---
+++ app/src/main/java/com/example/tryoutpas_absen5_absen11/DashboardFragment.java
@@ -0,0 +1,19 @@
+
+//INI UNTUK LALIGA
+package com.example.tryoutpas_absen5_absen11;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class DashboardFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return view;
+    }
+}
